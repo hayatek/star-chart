913a4e53be4062fbf3a73d8625634d0687e5ef49
hayatek <stream.hyt@gmail.com>
2019-06-09 09:51:52 +0900

    Repository model created

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Repository(models.Model):
+    class Meta:
+        db_table = 'repository'
+
+    database_id = models.IntegerField(null=False)
+    name_owner = models.CharField(max_length=100)
+    avatar_url = models.CharField(max_length=255)
+    resource_path = models.CharField(max_length=255)
+    created_at_github = models.DateTimeField(
+            blank=True, null=True)
+    updated_at_github = models.DateTimeField(
+            blank=True, null=True)
+    fetched_at = models.DateTimeField(
+            default=timezone.now)
+    language = models.CharField(max_length=50)
+    star_count = models.IntegerField(null=False)
+
+    def __str__(self):
+        return self.title
