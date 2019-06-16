f993c21c4724225a8ff5032bb3e1872f2bf07d16
hayatek <stream.hyt@gmail.com>
2019-06-16 09:17:09 +0900

    Added graphql function and local_settings.

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@_vidys07@ou0@jm))y9d83^p1mkd_t!erj(%w8)13-0td#0+9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ app/core/get_graphql.py
@@ -0,0 +1,54 @@
+import requests
+from app.local_settings import *
+
+
+class GraphQL:
+
+    headers = GITHUB_SECRET_KEY
+
+    """docstring for graphQL"""
+    def __init__(self):
+        super(GraphQL, self).__init__()
+
+    def run_query(self, query, variables):
+        request = requests.post('https://api.github.com/graphql',
+                                json={'query': query, 'variables': variables},
+                                headers=self.headers)
+        if request.status_code == 200:
+            return request.json()
+        else:
+            raise Exception("Query failed to run by returning code of {}. {}"
+                            .format(request.status_code, query))
+
+    def get_result(self):
+        query = '''
+            query($query_string:String!){
+                search( query:$query_string type: REPOSITORY, first: 1){
+                edges {
+                    node {
+                    ... on Repository {
+                    nameWithOwner
+                    owner{
+                    avatarUrl
+                        resourcePath
+                    }
+                    createdAt
+                    updatedAt
+                    primaryLanguage{
+                        name
+                    }
+                    stargazers{
+                        totalCount
+                    }
+                  }
+                }
+              }
+            }
+        }'''
+
+        variables = {
+            "query_string": "stars:>10000"
+        }
+
+        result = self.run_query(query, variables)  # execute query
+        print(result)
