6d21861d8f52ae6268d1437ee5a93ae4e8a996b5
hayatek <stream.hyt@gmail.com>
2019-07-06 22:50:25 +0900

    search app's model created.

---
+++ app/search/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Combination(models.Model):
+    class Meta:
+        db_table = 'combination'
+
+    database_id1 = models.IntegerField(default=0)
+    database_id2 = models.IntegerField(default=0,blank=True, null=True)
+    database_id3 = models.IntegerField(default=0,blank=True, null=True)
+    created_at = models.DateTimeField(
+            blank=True, null=True)
+    updated_at = models.DateTimeField(
+            blank=True, null=True)
+    access_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.repository1
