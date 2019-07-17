eff87083025fa73b75af4507853c286a2c39f00a
hayatek <stream.hyt@gmail.com>
2019-07-17 22:57:32 +0900

    made chart app and created template files.

---
+++ app/chart/urls.py
@@ -0,0 +1,10 @@
+'''
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # top
+    path('', views.index, name='index'),
+]
+'''
--- app/config/settings.py
+++ app/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'search',
+    'chart',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
