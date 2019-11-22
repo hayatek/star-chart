2bbba7ee46d68d685c0c585c692bfa74fbcc57e8
hayatek <stream.hyt@gmail.com>
2019-11-23 00:24:41 +0900

    update settings.py

--- app/config/celery.py
+++ app/config/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- app/config/settings/settings.py
+++ app/config/settings/settings.py
@@ -1,8 +1,6 @@
 import os
-
 from .base_settings import *
 
-ALLOWED_HOSTS = ['app']
+
 DEBUG = False
 PRODUCTION = True
-SECRET_KEY = os.environ.get('SECRET_KEY')
