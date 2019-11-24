c3ec64744aa2031fcb8fb9a6835e313583a26038
hayatek <stream.hyt@gmail.com>
2019-11-25 00:03:54 +0900

    test

---
+++ app/config/settings/settings_local.py
@@ -0,0 +1,9 @@
+from .base_settings import *
+import environ
+
+
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = env('SECRET_KEY')
+GITHUB_SECRET_KEY = env('GITHUB_SECRET_KEY')
