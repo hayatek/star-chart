2ef1ed74ee4e67e9b2cf078f12eebee2977c24bb
hayate <stream.hyt@gmail.com>
2019-11-27 22:10:58 +0900

    added logging

--- app/config/settings/settings.py
+++ app/config/settings/settings.py
@@ -11,6 +11,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 GITHUB_SECRET_KEY = os.environ['GITHUB_SECRET_KEY']
 
 #e-mail settings
+ADMINS = [('hayatek', 'over35man@gmail.com')]
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ['EMAIL_ADDRESS']
 EMAIL_HOST_PASSWORD = os.environ['PASS']
@@ -20,6 +21,23 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 #https settings
 #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 # update db settings
 db_from_env = dj_database_url.config(conn_max_age=400)
