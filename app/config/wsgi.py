fced30e6a67bd961147a013108016e046ffa867f
hayate <stream.hyt@gmail.com>
2019-11-28 22:43:02 +0900

    added awake

--- app/config/wsgi.py
+++ app/config/wsgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+from botocore.exceptions import ClientError
 from django.core.wsgi import get_wsgi_application
+import threading
+import requests
+import time
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
+
+def server_awake():
+    while True:
+        try:
+            requests.get("https://django-starchart.herokuapp.com//")
+        except ClientError as e:
+            print(e)
+        time.sleep(1200)
+
+t = threading.Thread(target=server_awake)
+t.start()
