338be70b0a379b520a1f4f422c1b59380b56d0b8
hayatek <stream.hyt@gmail.com>
2019-11-30 15:08:51 +0900

    update celery.py

--- app/config/celery.py
+++ app/config/celery.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from config.celery import Celery
+from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
 
