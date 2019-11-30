211d54468b59510a641ea8f127f608dbdda7f453
hayate <stream.hyt@gmail.com>
2019-11-30 15:46:15 +0900

    update celery.py

--- app/config/celery.py
+++ app/config/celery.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings')
-app.autodiscover_tasks()
+app.autodiscover_tasks(settings.INSTALLED_APPS)
 
 
 @app.task(bind=True)
