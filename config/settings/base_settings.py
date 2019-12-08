9e60b761e689df8b43ea3ab51fa18fcff697030f
hayate <stream.hyt@gmail.com>
2019-12-08 09:04:33 +0900

    directory structure changed

--- config/settings/base_settings.py
+++ config/settings/base_settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'chart',
-    'django_celery_beat',
-    'django_celery_results',
+#    'django_celery_beat',
+#    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -136,16 +136,15 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Local  Celery config
-CELERY_RESULT_BACKEND = 'django-db'
-CELERY_CACHE_BACKEND = 'django-cache'
-#CELERY_BROKER_URL = 'redis://redis:6379'
-CELERY_IMPORTS = ('core.tasks')
-#CELERY_RESULT_BACKEND = 'redis://redis:6379'
-'''
+#CELERY_RESULT_BACKEND = 'django-db'
+#CELERY_CACHE_BACKEND = 'django-cache'
+CELERY_BROKER_URL = 'redis://redis:6379'
+#CELERY_IMPORTS = ('core.tasks')
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+
 CELERY_BEAT_SCHEDULE = {
-    'hello': {
-        'task': 'core.tasks.hello',
-        'schedule': crontab()  # execute every minute
+    'update_database': {
+        'task': 'core.tasks.update_database',
+        'schedule': crontab()
     }
 }
-'''
