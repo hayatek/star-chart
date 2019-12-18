431769836af0cfce576c23cce1ae05cf1fcde084
hayate <stream.hyt@gmail.com>
2019-12-18 23:40:27 +0900

    update index.html

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -1,36 +1,78 @@
+import os
 from .base_settings import *
-import environ
+import dj_database_url
 
 
-env = environ.Env()
-env.read_env(os.path.join(BASE_DIR, '.env'))
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
-SECRET_KEY=env('SECRET_KEY')
-GITHUB_SECRET_KEY=env('GITHUB_SECRET_KEY')
+# secret key settings
+SECRET_KEY = os.environ['SECRET_KEY']
+GITHUB_SECRET_KEY = os.environ['GITHUB_SECRET_KEY']
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
-    'chart',
-    'django_celery_beat',
-    'django_celery_results',
+# e-mail settings
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = os.environ['EMAIL_ADDRESS']
+#EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#SERVER_EMAIL = EMAIL_HOST_USER
+
+# https settings
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'config.settings.context_processors.get_tracking_id',
+            ],
+        },
+    },
 ]
 
-#Local  Celery config for django-celery-beat
-#CELERY_RESULT_BACKEND = 'django-db'
-#CELERY_CACHE_BACKEND = 'django-cache'
-#BROKER_URL = 'redis://redis:6379'
+GOOGLE_ANALYTICS_KEY = os.environ['GOOGLE_ANALYTICS_KEY']
+
+# db settings
+db_from_env = dj_database_url.config(conn_max_age=400)
+DATABASES['default'].update(db_from_env)
+
+# Whitenoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# Celery config for production
+BROKER_URL = os.environ['REDIS_URL']
+CELERY_RESULT_BACKEND = os.environ["REDIS_URL"]
+BROKER_POOL_LIMIT = 1
+BROKER_CONNECTION_MAX_RETRIES = None
 CELERY_IMPORTS = ('core.tasks')
-
-#Local  Celery config for celery beat
-BROKER_URL = 'redis://redis:6379'
-CELERY_RESULT_BACKEND = 'redis://redis:6379'
-
-#FIXTURE_DIRS = (
-#   os.path.join(BASE_DIR, 'fixtures'),
-#)
+CELERY_BEAT_SCHEDULE = {
+    'update_database': {
+        'task': 'core.tasks.update_database',
+        'schedule': crontab(minute=0, hour=15) #must be UTC(JST-9h)
+    }
+}
